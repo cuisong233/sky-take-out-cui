9cd1c2c7a9fda63eebc4a3103af03bb518a78791
cuisong233 <qingsong233@hotmail.com>
2024-06-03 15:18:38 +0800

    新增员工、员工分页查询代码开发

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,17 +1,28 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
 import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
+import com.sky.result.PageResult;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -40,6 +51,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //密码比对
         // TODO 后期需要进行md5加密，然后再进行比对
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
         if (!password.equals(employee.getPassword())) {
             //密码错误
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
@@ -52,6 +64,43 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //3、返回实体对象
         return employee;
+    }
+
+    @Override
+    public void insertEmp(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+
+        //从DTO类总拷贝到entry类中
+        BeanUtils.copyProperties(employeeDTO, employee);
+
+        //增加其他默认数据
+        //账号状态
+        employee.setStatus(1);
+        //账号密码
+        employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
+        //创建时间 更新时间 创建人 更新人
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        employee.setCreateUser(BaseContext.getCurrentId());
+        employee.setUpdateUser(BaseContext.getCurrentId());
+
+        employeeMapper.insertEmp(employee);
+    }
+
+    @Override
+    public PageResult searchEmpPage(EmployeePageQueryDTO employeePageQueryDTO) {
+        //根据条件查询数据库-在Mapper中操作
+        // 设置page
+//        employeePageQueryDTO.setPage((employeePageQueryDTO.getPage()-1)*employeePageQueryDTO.getPageSize());
+        PageHelper.startPage(employeePageQueryDTO.getPage(), employeePageQueryDTO.getPageSize());
+//
+//        List<Employee> employee = employeeMapper.searchEmpPage(employeePageQueryDTO);
+//        Integer total = employeeMapper.searchEmpPageTotal(employeePageQueryDTO);
+
+        Page<Employee> page = employeeMapper.searchEmpPage(employeePageQueryDTO);
+
+        return new PageResult(page.getTotal(), page.getResult());
     }
 
 }
